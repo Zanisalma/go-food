c8237cd09ca590847c49a8707e04dafad4bd9e6b
zanisalma <zanisalma@gmail.com>
2018-08-14 05:47:16 +0000

    a new commit nambahin test

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -4,6 +4,32 @@ describe Food do
     it "has a valid factory" do
         expect(FactoryGirl.build(:food)).to be_valid
     end
+  
+    it "is valid with numeric price greater or equal to 0.01" do
+      expect(build(:food, price: 0.01)).to be_valid
+    end
+  
+    it "is invalid without numeric price" do
+      food = build(:food, price: "abc")
+      food.valid?
+      expect(food.errors[:price]).to include("is not a number")
+    end
+  
+    it "is invalid with price less than 0.01" do
+      food = build(:food, price: -10)
+      food.valid?
+      expect(food.errors[:price]).to include("must be greater than or equal to 0.01")
+    end
+
+    it "is valid with image_url ending with '.gif', '.jpg', or '.png'" do
+      expect(build(:food, image_url: "food.jpg")).to be_valid
+    end
+  
+    it "is invalid with image_url ending not with '.gif', '.jpg', or '.png'" do
+      food = build(:food, image_url: "food.csv")
+      food.valid?
+      expect(food.errors[:image_url]).to include("must be a URL for GIF, JPG or PNG image.")
+    end
 
   it "is valid with a name and description" do
     # food = Food.new(
@@ -122,5 +148,26 @@ describe Food do
         expect(Food.by_letter("N")).not_to include(@food2)
       end
     end
+
+    describe "filter name by letter" do
+      # before :each do
+      #   @food1 = create(:food, name: "Nasi Uduk")
+      #   @food2 = create(:food, name: "Kerak Telor")
+      #   @food3 = create(:food, name: "Nasi Semur Jengkol")
+      # end
+  
+      context "with matching letters" do
+        it "returns a sorted array of results that match" do
+          expect(Food.by_letter("N")).to eq([@food3, @food1])
+        end
+      end
+  
+      context "with non-matching letters" do
+        it "omits results that do not match" do
+          expect(Food.by_letter("N")).not_to include(@food2)
+        end
+      end
+    end
+    
   end
 end
